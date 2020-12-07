7a5b57674623f159f010411de5ed5db05e2b53f7
Damaris <d4maris@devisland.de>
2020-12-07 16:55:09 +0100

    first

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,160 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react'
+import  '../styles/globals.css'
+import Carousel from "react-multi-carousel";
+import 'react-multi-carousel/lib/styles.css'
+import MobileDetect from "mobile-detect";
+import { withStyles } from "@material-ui/core/styles";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import Typography from "@material-ui/core/Typography";
+import Card from "../components/card";
+import Image from "../components/image";
+import CarouselWithCustomDots from "../components/carousel-with-custom-dots";
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+class Index extends React.Component {
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+  static getInitialProps({ req }) {
+    let userAgent;
+    let deviceType;
+    if (req) {
+      userAgent = req.headers["user-agent"];
+    } else {
+      userAgent = navigator.userAgent;
+    }
+    const md = new MobileDetect(userAgent);
+    if (md.tablet()) {
+      deviceType = "tablet";
+    } else if (md.mobile()) {
+      deviceType = "mobile";
+    } else {
+      deviceType = "desktop";
+    }
+    return { deviceType };
+  }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+  state = { isMoving: false };
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+  render() {
+
+    const {classes} = this.props;
+
+    const images = [
+      "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    ];
+
+    const texts = [
+      "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript.",
+      "Fixing CSS load order/style.chunk.css incorrect in Nextjs",
+      "React Carousel with Server Side Rendering Support – Part 1",
+      "React Carousel with Server Side Rendering Support – Part 2",
+      "Flutter Xcode couldn’t find any iOS App Development provisioning profiles"
+    ];
+
+    const fakerData = Array(12)
+        .fill(0)
+        .map((item, index) => {
+          return {
+            image: images[index],
+            headline: "w3js -> web front-end studio",
+            description: texts[index] || texts[0]
+          };
+        });
+    const responsive = {
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3,
+        slidesToSlide: 3
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        slidesToSlide: 2
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        slidesToSlide: 1
+      }
+    };
+    return (
+        <div className={classes.root}>
+          <Typography className={classes.title} variant="h6" color="grey">
+            <a target="_blank" href="https://w3js.com/">
+              A Carousel supports multiple items and server-side rendering
+            </a>
+          </Typography>
+          <Typography className={classes.title} variant="p" color="grey">
+            <a target="_blank" href="https://w3js.com/">
+              This is the server-side rendering demo of the libiary, try to
+              disable the JavaScript in your browser, you will still see our
+              Carousel renders nicely
+            </a>
+          </Typography>
+
+          //First slider
+          <Carousel
+
+               swipeable={false}
+               draggable={false}
+
+              responsive={responsive}
+              ssr
+              infinite={false}
+              beforeChange={() => this.setState({ isMoving: true })}
+              afterChange={() => this.setState({ isMoving: false })}
+              containerClass="first-carousel-container container"
+              deviceType={this.props.deviceType}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            {fakerData.map(card => {
+              return <Card isMoving={this.state.isMoving} {...card} />;
+            })}
+          </Carousel>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+          //Second Slider
+          <Carousel
+              /*
+              swipeable={false}
+              draggable={false}
+              */
+              responsive={responsive}
+              ssr
+              showDots
+              infinite
+              containerClass="container-with-dots"
+              itemClass="image-item"
+              deviceType={this.props.deviceType}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            {fakerData.slice(0, 5).map(card => {
+              return <Image url={card.image} alt={card.headline} />;
+            })}
+          </Carousel>
+          //3rd Slider
+          <CarouselWithCustomDots deviceType={this.props.deviceType} />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
+  }
 }
+
+const styles = () => ({
+  root: {
+    textAlign: "center"
+  },
+  title: {
+    maxWidth: 400,
+    margin: "auto",
+    marginTop: 10
+  }
+});
+
+export default withStyles(styles)(Index);
